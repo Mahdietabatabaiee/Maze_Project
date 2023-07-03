31f599f5e7ce7196ab5386a2c4f2149df77177a0
Mahdietabatabaiee <mahd.tabatabaee@gmail.com>
2023-07-03 23:55:51 +0430

    Add project files.

---
+++ maze 2/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace maze_2
+{ 
+    //  |  = دیوار ها با  
+    //  مسیر خالی = راه بازی 
+    // X = راه حل بازی 
+    // شروع و پایان مسیر = 0
+    // مهدیه سادات طباطبایی - ماز 50 در 50 رندوم
+    class Program
+    {
+        static int x= 50;
+        static int y = 50;
+        static char[,] maze = new char[x, y];
+        static bool[,] check = new bool[x, y];
+        static int startx = 0;
+        static int starty = 0;
+        static int endx = x - 1;
+        static int endy = y - 1;
+
+        static void Main(string[] args)
+        {
+            GrMaze();
+            var maz = new maze1(x,y,maze);
+            maz.ShowMaze();
+
+           
+            if (Findway(startx, starty))
+            {
+                Console.WriteLine("\n\n The soloution found  :) !! \n\n");
+                var m=new maze1(x,y,maze);
+                m.ShowMaze();
+               
+            }
+            else
+            {
+                Console.WriteLine("No soloution found :(");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void GrMaze()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    if (random.Next(0, 100) < 30) 
+                    {
+                        maze[i, j] = '|';
+                    }
+                    else
+                    {
+                        maze[i, j] = ' ';
+                    }
+                }
+            }
+
+            maze[startx, starty] = '0';
+            maze[endx, endy] = '0';
+        }
+
+ 
+
+        static bool Findway(int row, int col)
+        {
+            if (row < 0 || row >= x || col < 0 || col >= y || maze[row, col] == '|' || check[row, col])
+            {
+                return false;
+            }
+
+            check[row, col] = true;
+
+            if (row == endx && col == endy)
+            {
+                return true;
+            }
+
+            if (Findway(row - 1, col)) 
+            {
+                maze[row, col] = 'X';
+                return true;
+            }
+
+            if (Findway(row + 1, col)) 
+            {
+                maze[row, col] = 'X';
+                return true;
+            }
+
+            if (Findway(row, col - 1)) 
+            {
+                maze[row, col] = 'X';
+                return true;
+            }
+
+            if (Findway(row, col + 1))
+            {
+                maze[row, col] = 'X';
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+    
---
+++ maze 2/maze1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maze_2
+{
+    internal class maze1
+    {
+         static private int x = 50;
+       static private int y = 50;
+       static private char[,] maze = new char[x,y];
+        public maze1() { }
+        public maze1(int X, int Y, char[,] Maze)
+        {
+            x= X;
+            y = Y;
+           maze = Maze;
+
+        }
+        public void ShowMaze()
+        {
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write(maze[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
